43cf623cc66875230bebdd64a30ed0d2a4a3fb9d
Jela Antoinette Jeminez <jelajeminez8@gmail.com>
2024-08-25 01:03:42 +0800

    Add files via upload
    adding some codes

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
- 
-using namespace std;
-	
-int main(){
-	
-	int choice;
-		do
-{
-		cout << endl
-		<< " List files in the current directory\n"
-		<< " Create a new directory\n"
-		<< " Change the working directory\n"
-		<< " Exit the program.\n"
-		<< " Enter your choice and press return: ";
-		cin >> choice;
 
-			return 0;
-}
+using namespace std;
+
+int main() {
+    int choice;
+
+    do {
+        cout << endl
+             << "Main Menu\n"
+             << "------------------------\n"
+             << "1 - List files in the current directory\n"
+             << "2 - Create a new directory\n"
+             << "3 - Change the working directory\n"
+             << "4 - Exit the program\n"
+             << "Enter your choice and press return: ";
+        cin >> choice;
+
+
