6719194425386984d14d22437826b742658086d6
Jela Antoinette Jeminez <jelajeminez8@gmail.com>
2024-08-25 22:08:00 +0800

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cstdlib>  
+#include <string>
 
 using namespace std;
-
 
 void showDirectoryMenu() {
     int dirChoice;
@@ -45,6 +45,49 @@ void showDirectoryMenu() {
     }
 }
 
+void changeDirectoryMenu() {
+    int changeDirChoice;
+    string directory;
+
+    cout << endl
+         << "----------------------\n"
+         << "CHANGE DIRECTORY MENU\n"
+         << "----------------------\n"
+         << "1 - Move one step back (to the parent directory)\n"
+         << "2 - Move to the root directory\n"
+         << "3 - Move to a specific directory\n"
+         << "Enter your choice and press return: ";
+    cin >> changeDirChoice;
+
+    switch (changeDirChoice) {
+        case 1: {
+            cout << "Moving one step back...\n";
+            system("cd ..");
+            break;
+        }
+        case 2: {
+            cout << "Moving to the root directory...\n";
+            system("cd \\");
+            break;
+        }
+        case 3: {
+            cout << "Enter the directory path: ";
+            cin.ignore();  
+            getline(cin, directory);
+            string command = "cd \"" + directory + "\"";
+            if (system(command.c_str()) == 0) {
+                cout << "Successfully moved to: " << directory << endl;
+            } else {
+                cout << "Failed to move to: " << directory << endl;
+            }
+            break;
+        }
+        default:
+            cout << "Invalid choice in change directory menu.\n";
+            break;
+    }
+}
+
 int main() {
     int choice;
 
@@ -62,14 +105,14 @@ int main() {
 
         switch (choice) {
             case 1: {
-                showDirectoryMenu(); 
+                showDirectoryMenu();
                 break;
-            } 
+            }
             case 2:
                 cout << "Creating new directory.....>\n";
                 break;
             case 3:
-                cout << "Changing working directory......\n";
+                changeDirectoryMenu();
                 break;
             case 4:
                 cout << "Exiting the program...\n";
