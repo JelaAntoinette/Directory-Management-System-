cfe1c0d05066f7963d36848d6a2a777d2b224429
Jela Antoinette Jeminez <jelajeminez8@gmail.com>
2024-08-25 18:35:11 +0800

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 using namespace std;
+void showFileListMenu();
 
 int main() {
     int choice;
 
     do {
         cout << endl
-             << "Main Menu\n"
-             << "------------------------\n"
+        	<< " ---------------\n"
+             << "MAIN MENU\n"
+             << "---------------\n"
              << "1 - List files in the current directory\n"
              << "2 - Create a new directory\n"
              << "3 - Change the working directory\n"
@@ -16,4 +18,26 @@ int main() {
              << "Enter your choice and press return: ";
         cin >> choice;
 
+ switch (choice) {
+            case 1: {
+                showFileListMenu();
+                break;
+            case 2:
+        		cout << "Creating new directory.....>\n";
+        		break;
+        	case 3:
+        		cout << "Changing working directory......\n";
+        		break;
+        	case 4:
+        		cout << "Press any key to ext\n";
+        		break;
+        	default:
+        		cout << "Invalid choice. Please try again";
+        		break;
 
+            
+        }
+    } while (choice != 4);
+
+    return 0;
+	}
